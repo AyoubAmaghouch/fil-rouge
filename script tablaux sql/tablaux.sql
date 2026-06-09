800964e3fc7d0491e8488fad4890fb478de79620
ayoub <ayoubamaghouche@gmail.com>
2026-06-09 23:48:56 +0100

    Add localisation and whatsapp fields to agences table; create admins table for admin management

--- script tablaux sql/tablaux.sql
+++ script tablaux sql/tablaux.sql
@@ -3,10 +3,18 @@ CREATE TABLE agences (
     nom_agence VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     telephone VARCHAR(20) NOT NULL,
+    whatsapp VARCHAR(20),
     ville VARCHAR(100) NOT NULL,
+    localisation VARCHAR(255),
     mot_de_passe VARCHAR(255) NOT NULL,
     logo VARCHAR(255) NOT NULL,
     statut_validation BOOLEAN DEFAULT 0
+);
+
+CREATE TABLE admins (
+    id_admin INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    mot_de_passe VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE marques (
@@ -24,8 +32,11 @@ CREATE TABLE voitures (
     prix DECIMAL(10,2) NOT NULL,
     disponibilite BOOLEAN NOT NULL,
 
-    FOREIGN KEY (id_agence) REFERENCES agences(id_agence),
-    FOREIGN KEY (id_marque) REFERENCES marques(id_marque)
+    FOREIGN KEY (id_agence)
+    REFERENCES agences(id_agence),
+
+    FOREIGN KEY (id_marque)
+    REFERENCES marques(id_marque)
 );
 
 CREATE TABLE images_voitures (
@@ -33,5 +44,23 @@ CREATE TABLE images_voitures (
     id_voiture INT NOT NULL,
     image VARCHAR(255) NOT NULL,
 
-    FOREIGN KEY (id_voiture) REFERENCES voitures(id_voiture)
+    FOREIGN KEY (id_voiture)
+    REFERENCES voitures(id_voiture)
+);
+
+
+
+INSERT INTO marques (nom_marque) VALUES
+('BMW'),
+('Audi'),
+('Mercedes'),
+('Peugeot'),
+('Renault'),
+('Volkswagen'),
+('Toyota'),
+('Hyundai');
+INSERT INTO admins (email, mot_de_passe)
+VALUES (
+'ayoubamaghouche@gmail.com',
+'$2y$12$2Bw4.PldiPHRk.aYnLEsCeAM42Uv37SJ1zuiDrpD1QBHTw7mZEAM2'
 );
